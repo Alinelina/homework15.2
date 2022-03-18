588d87e47e01f8d298c29d44160d77875de433a8
alinelina <alinelina2013@gmail.com>
2022-03-18 13:05:38 +0200

    add show password and red border on error

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-let  select = (sender) => {
+let select = (sender) => {
     sender.style.backgroundColor = '#dfdddd';
 };
 
@@ -6,23 +6,30 @@ let userName = document.getElementById('userName');
 let userSecondName = document.getElementById('userSecondName');
 let errorNameMessage = document.getElementById('userError');
 
-function checkName() {
+let checkName = () => {
     if (userName.value == '' && userSecondName.value == '') {
-        errorNameMessage.innerHTML = 'Заполните поля Имя и Фамилия';
+        errorNameMessage.innerHTML = 'Введите имя и фамилию';
+        userName.style.borderColor = '#ea4335';
+        userSecondName.style.borderColor = '#ea4335';
     } else if (userName.value == '') {
-        errorNameMessage.innerHTML = 'Заполните поле Имя';
+        errorNameMessage.innerHTML = 'Укажите имя';
+        userName.style.borderColor = '#ea4335';
     } else if (userSecondName.value == '') {
-        errorNameMessage.innerHTML = 'Заполните поле Фамилия';
+        errorNameMessage.innerHTML = 'Укажите фамилию';
+        userSecondName.style.borderColor = '#ea4335';
     } else {
         errorNameMessage.innerHTML = '';
-    }  
+    }
 };
 
 let userLogin = document.getElementById('mail');
 let userLoginError = document.getElementById('userLoginError');
-function checkLogin() {
+let userLoginBorder = document.querySelector('.mail-container');
+
+let checkLogin = () => {
     if (userLogin.value == '') {
-        userLoginError.innerHTML = 'Заполните имя пользователя';
+        userLoginError.innerHTML = 'Укажите адрес Gmail';
+        userLoginBorder.style.borderColor = '#ea4335';
     } else {
         userLoginError.innerHTML = '';
     }
@@ -31,18 +38,43 @@ function checkLogin() {
 let userPassword = document.getElementById('password');
 let repeatPassword = document.getElementById('repeatPassword');
 let userPasswordError = document.getElementById('userPasswordError');
-function checkPassword() {
-    if (userPassword.value == '' && repeatPassword.value == '') {
-        userPasswordError.innerHTML = 'Заполните поля Пароль и Подтвердить пароль';
-    } else if (userPassword.value == '') {
-        userPasswordError.innerHTML = 'Заполните поле Пароль';
+
+let checkPassword = () => {
+    if (userPassword.value == '') {
+        userPasswordError.innerHTML = 'Введите пароль';
+        userPassword.style.borderColor = '#ea4335';
     } else if (repeatPassword.value == '') {
-        userPasswordError.innerHTML = 'Заполните поле Подтвердить пароль';
+        userPasswordError.innerHTML = 'Подтвердите пароль';
+        repeatPassword.style.borderColor = '#ea4335';
     } else {
         userPasswordError.innerHTML = '';
-    }  
+    }
 };
 
 checkFormName.addEventListener('click', checkName);
 checkFormName.addEventListener('click', checkLogin);
 checkFormName.addEventListener('click', checkPassword);
+
+const togglePassword = document.getElementById('show-password');
+
+const showOrHidePassword = () => {
+    const password = document.getElementById('password');
+    const repeatPassword = document.getElementById('repeatPassword');
+    if (password.type === 'password' || repeatPassword.type === 'password') {
+        password.type = 'text';
+    } else {
+        password.type = 'password';
+    }
+};
+
+const showOrHideRepeatPassword = () => {
+    const repeatPassword = document.getElementById('repeatPassword');
+    if (repeatPassword.type === 'password') {
+        repeatPassword.type = 'text';
+    } else {
+        repeatPassword.type = 'password';
+    }
+};
+
+togglePassword.addEventListener('change', showOrHidePassword);
+togglePassword.addEventListener('change', showOrHideRepeatPassword);
